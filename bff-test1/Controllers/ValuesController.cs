1de1905a835edfbce2a95fef295d992f7dc77f59
ALEJANDRO HENAO LOAIZA <Alejandro.Henao.Loaiza@epm.com.co>
2018-10-05 16:48:54 -0500

    Intento de uso de SendAsync (incompleto)

--- bff-test1/Controllers/ValuesController.cs
+++ bff-test1/Controllers/ValuesController.cs
@@ -21,36 +21,40 @@ namespace bff_test1.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        private static async Task<String> CallService()
+        private static async Task<HttpResponseMessage> CallService()
         {
             var handler = new HttpClientHandler();
-            handler.Proxy = new WebProxy("http://proxy.epm.com.co:8080", true){ UseDefaultCredentials = true };
+            handler.Proxy = new WebProxy("http://proxy.epm.com.co:8080", true) { UseDefaultCredentials = true };
             HttpClient client = new HttpClient(handler);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("User-Agent", "Transactional Portal");
 
-            try
-            {
-                var stringTask = client.GetStringAsync(
-                    "http://localhost:8080/userdataaccess/get?email=alejo.catson@gmail.com");
-                var msg = await stringTask;
-                return msg;
-            }
-            catch(HttpRequestException e)
-            {
-                return e.Message;
-            }
-
+            // var stringTask = client.GetStringAsync(
+            //     "http://localhost:8080/userdataaccess/get?email2=alejo.catson@gmail.com");
+            // var msg = await stringTask;
+            // return msg;
+            var response = await client.SendAsync(
+                new HttpRequestMessage(HttpMethod.Get,
+                    new Uri("http://localhost:8080/userdataaccess/get?email=alejo.catson@gmail.com")));
+            return response;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<Object> Get(int id)
+        public ActionResult<String> Get(int id)
         {
-            var val1 = CallService().Result;
-            Console.WriteLine(JsonConvert.DeserializeObject(val1));
-            return JsonConvert.DeserializeObject(val1);
+            var content = CallService().Result.Content;
+            return "hola";
+            // try
+            // {
+            //     return JsonConvert.DeserializeObject(CallService().Result);
+            // }
+            // catch (HttpRequestException e)
+            // {
+            //     Console.WriteLine("----> " + e.Data.Keys);
+            //     return StatusCode(500);
+            // }
         }
 
         // POST api/values
